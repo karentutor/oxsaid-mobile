101f315d9763b2ff1bfbc8b5795856da11dd7ddc
Karen <karentutor1@gmail.com>
2024-08-13 13:48:04 -0700

    default screens

--- utils/useTailwindResponsive.js
+++ utils/useTailwindResponsive.js
@@ -1,7 +1,15 @@
 // useTailwindResponsive.js
 import { useState, useEffect } from "react";
 import { Dimensions } from "react-native";
-import tw from "../tailwind";
+import tw from "./tailwind";
+
+// Manually define screen sizes as per your Tailwind configuration
+const screens = {
+  sm: 640,
+  md: 768,
+  lg: 1024,
+  xl: 1280,
+};
 
 export default function useTailwindResponsive() {
   const [screenWidth, setScreenWidth] = useState(
@@ -22,15 +30,15 @@ export default function useTailwindResponsive() {
 
   const styles = {
     indexGrid:
-      screenWidth >= parseInt(tw.theme.screens.xl)
+      screenWidth >= screens.xl
         ? tw`flex-row justify-center mx-auto`
-        : screenWidth >= parseInt(tw.theme.screens.lg)
+        : screenWidth >= screens.lg
         ? tw`flex-col`
         : tw`flex-col`,
     twoColumnGrid:
-      screenWidth >= parseInt(tw.theme.screens.xl)
+      screenWidth >= screens.xl
         ? tw`flex-row justify-center mx-auto`
-        : screenWidth >= parseInt(tw.theme.screens.lg)
+        : screenWidth >= screens.lg
         ? tw`flex-col`
         : tw`flex-col`,
   };
