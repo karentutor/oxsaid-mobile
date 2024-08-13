2fe03b9ee0286c9fb43767faacc3f2a5fa55d54a
Karen <karentutor1@gmail.com>
2024-08-13 16:22:15 -0700

    added fonts

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Toast from "react-native-toast-message";
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading"; // Optional: To handle the loading state
+
 import { AuthProvider } from "./context"; // Ensure correct import path for AuthProvider
 
 // Import your screens
-import Landing from "./pages/Landing"; // import Join from './pages/auth/Join';
+import Landing from "./pages/Landing";
+// import Join from './pages/auth/Join';
 // import Register from './pages/auth/Register';
 // import Signin from './pages/auth/Signin';
 import Home from "./pages/Home";
@@ -15,6 +19,23 @@ import Settings from "./pages/Settings";
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  const loadFonts = async () => {
+    await Font.loadAsync({
+      "Rubik-Regular": require("./assets/fonts/Rubik-Regular.ttf"),
+    });
+    setFontsLoaded(true);
+  };
+
+  useEffect(() => {
+    loadFonts();
+  }, []);
+
+  if (!fontsLoaded) {
+    return <AppLoading />; // Display a loading screen until fonts are loaded
+  }
+
   return (
     <AuthProvider>
       <NavigationContainer>
